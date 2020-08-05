f0c03a81a854cb43d87f7e12bca0e2273cce8d60
Hai Bui Vu <buivuhai.bk@gmail.com>
2020-08-05 22:05:40 +0700

    Start with react router

---
+++ react-redux-saga/src/containers/Users.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Users = props => {
+  return (
+    <div>
+      <p>Users</p>
+    </div>
+  )
+}
+
+Users.propTypes = {
+
+}
+
+export default Users
